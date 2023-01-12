e492fb35dd91d3efdb13486c7401b3e75de0089b
Ducter-dev <michel.morales@ducter.com.mx>
2023-01-12 13:12:35 -0600

    :sparkles: Añadir front para recuperar contraseña

---
+++ src/components/Forms/FormLogin.vue
@@ -0,0 +1,34 @@
+<template>
+    <form class="w-3/5 md:w-4/12" @submit.prevent="onSubmit">
+      <div class="mb-6">
+        <label for="usuario" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Usuario</label>
+        <input type="text" id="usuario" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Nombre de Usuario" required>
+      </div>
+      <div class="mb-6">
+        <label for="pass" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Contraseña</label>
+        <input type="password" id="pass" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Contraseña" required>
+      </div>
+      <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+        <span class="font-medium">Error!</span> {{ mensaje }}
+      </div>
+      <div class="flex justify-center items-center">
+        <button type="submit" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Iniciar Sesión</button>
+      </div>
+      <div class="flex justify-center items-center mt-6 text-center ">
+        <router-link to="/password/recuperar">
+          <p class="text-base underline underline-offset-2">Olvidé mi contraseña</p>
+        </router-link>
+      </div>
+    </form>
+</template>
+
+<script>
+    import { ref, reactive } from 'vue'
+    import useAuth from '../../modules/auth/composables/auth'
+    
+    export default {
+      setup(props, context) {
+        return {}
+      }
+    }
+</script>
---
+++ src/views/RecuperarPassword.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container mx-auto h-full flex flex-col justify-center items-center">
+      <div class="w-full flex justify-center items-center mb-10">
+        <div class="justify-center items-center">
+          <img class="w-65 md:w-100" alt="Logo Termigas">
+          <div class="flex justify-center items-center">
+            <p class="font-bold text-lg text-dark">Recuperar Contraseña</p>
+          </div>
+        </div>
+      </div>
+      <div class="w-full flex justify-center items-center">
+        <FormRecuperarPassword />
+      </div>
+    </div>
+</template>
+  
+<script>
+  import LogoTermigas from '../assets/logo.png'
+  import FormRecuperarPassword from '../components/Forms/FormRecuperarPassword.vue'
+  
+  export default {
+    components: { LogoTermigas, FormRecuperarPassword },
+    setup() {
+      return {
+        LogoTermigas,
+      }
+    }
+  }
+</script>
